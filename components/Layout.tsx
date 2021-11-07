0f31cf4f7f7821b2f50e74c852d8830dde159b52
kazoo1122 <kazoo1122@experienced.work>
2021-11-07 14:58:54 +0900

    Layout.tsx modified.

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,16 +1,17 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const Layout = (props: any) => {
     const { pageTitle, pageUrl, pageDescription, children } = props;
-    const defaultTitle = 'レジ打ちからエンジニアになりました';
+    const siteTitle = 'レジ打ちからエンジニアになりました';
     const defaultDescription =
         'ショップ店員から色んな経験を経て中途のITエンジニアになった人のブログです';
-    const title = pageTitle ? `${pageTitle} | ${defaultTitle}` : defaultTitle;
+    const title = pageTitle ? `${pageTitle} | ${siteTitle}` : siteTitle;
     const description = pageDescription
         ? `${pageDescription} | ${defaultDescription}`
         : defaultDescription;
-    const ogType = pageTitle === defaultTitle ? 'blog' : 'article';
+    const ogType = pageTitle ? 'article' : 'blog';
 
     return (
         <div className='page'>
@@ -34,7 +35,33 @@ const Layout = (props: any) => {
                 <meta name='twitter:site' content='@kazoo_1122' />
             </Head>
 
-            <header></header>
+            <header>
+                <div className='headerBox'>
+                    <h1>レジ打ちからエンジニアになりました</h1>
+                    <p className='subHeddings'>〜中途エンジニアの開発日誌〜</p>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link href='/index'>BLOG</Link>
+                            </li>
+                            <li>
+                                <Link href='/profile'>PROFILE</Link>
+                            </li>
+                            <li>
+                                <Link href='/contact'>CONTACT</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <Image src='/images/dot_background.png' alt='header background' />
+            </header>
+
+            <main>
+                {pageTitle ? <h2 className='page-title'>{title}</h2> : ``}
+                <div className='page-main'>{children}</div>
+            </main>
+
+            <footer>&copy; 2021 {siteTitle}</footer>
         </div>
     );
 };
