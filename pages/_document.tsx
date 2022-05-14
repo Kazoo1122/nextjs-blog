55c6ac6893aef5613c9506ab7662ea6ef08e5f78
kazoo1122 <kazoo1122@experienced.work>
2022-05-14 19:53:35 +0900

    favicon関係修正

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,6 +17,27 @@ class MyDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=RocknRoll+One&family=Spartan:wght@400;500;800&family=Yusei+Magic&display=swap'
             rel='stylesheet'
           />
+          <link
+            rel='apple-touch-icon'
+            sizes='180x180'
+            href='/images/favicon/apple-touch-icon.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='32x32'
+            href='/images/favicon/favicon-32x32.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='16x16'
+            href='/images/favicon/favicon-16x16.png'
+          />
+          <link rel='manifest' href='/images/favicon/site.webmanifest' />
+          <link rel='mask-icon' href='/images/favicon/safari-pinned-tab.svg' color='#5bbad5' />
+          <meta name='msapplication-TileColor' content='#da532c' />
+          <meta name='theme-color' content='#ffffff' />
 
           {/* gtag / Google Analyticsの利用*/}
           {GA_TRACKING_ID && (
@@ -37,7 +58,6 @@ class MyDocument extends Document {
               }}
             />
           )}
-          <link rel='apple-touch-icon' sizes='180x180' href='/images/apple_touch_icon.png' />
         </Head>
         <body>
           <Main />
