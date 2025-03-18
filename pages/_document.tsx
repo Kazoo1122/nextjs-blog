9d6afad1690e4448d12806ffa717b282a08eb4e0
kazoo1122 <syayo181@gmail.com>
2025-03-18 15:01:10 +0900

    update: ga tracking id

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -46,20 +46,18 @@ class MyDocument extends Document {
 
           {/* gtag / Google Analyticsの利用*/}
           {GA_TRACKING_ID && (
-            <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+            <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}></script>
           )}
           {GA_TRACKING_ID && (
             <script
               dangerouslySetInnerHTML={{
                 __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
 
-              gtag('config', '${GA_TRACKING_ID}', {
-                page_path: window.location.pathname,
-              });
-            `,
+                gtag('config', '${GA_TRACKING_ID}');
+              `,
               }}
             />
           )}
